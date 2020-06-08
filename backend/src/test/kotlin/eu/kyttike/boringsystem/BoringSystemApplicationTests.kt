f8f5e9287e903a05266292ad805c269cb8f7fd18
kyttike <danielkytt@hotmail.com>
2020-06-09 00:06:26 +0300

    Project initialization
    Spring boot project with elementary postgresql configuration. Also has a simple editor config for consistency.

---
+++ backend/src/test/kotlin/eu/kyttike/boringsystem/BoringSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package eu.kyttike.boringsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoringSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
